4a2e2bb883890a1ed8b73d531eb965a633e78a34
raccooccoon <zavadskaya_91@mail.ru>
2019-02-26 01:05:51 +0300

    ghh

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@
 module.exports = function getLoveTrianglesCount(preferences = []) {
   let count = 0;
   for (let i = 0; i < preferences.length; i++) {
-    if ((preferences[i] !== i + 1) && (preferences[i + 1] !== i + 2) && (preferences[i + 2] !== i + 3) && (preferences[i] + preferences[i + 1] + preferences[i + 2] == 3 * i + 6)) {
-    count++;
+    if (preferences[i] != i + 1 && preferences[i + 1] != i + 2 && preferences[i + 2] !== i + 3 && preferences[i] + preferences[i + 1] + preferences[i + 2] == 3 * i + 6) {
+    count += 1;
     }
   }
   return count;
